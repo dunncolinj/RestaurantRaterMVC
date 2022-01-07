0d02b619c0f6c3b12600cd5ef598bf96a09d8b4a
Colin Dunn <dunncolinj@gmail.com>
2022-01-06 20:08:35 -0500

    Set up DB layer

---
+++ RestaurantRaterMVC/Models/Restaurant.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRaterMVC.Models
+{
+    public class Restaurant
+    {
+        [Key]
+        public int RestaurantId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Rating { get; set; }
+    }
+
+    public class RestaurantDbContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
